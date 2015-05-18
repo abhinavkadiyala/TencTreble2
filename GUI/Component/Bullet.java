e7c029dcdba11d3d82230ba5a20c1c9f24732524
justindli <justindli@users.noreply.github.com>
2015-05-18 09:32:01 -0700

    Update Bullet.java

--- GUI/Component/Bullet.java
+++ GUI/Component/Bullet.java
@@ -8,6 +8,7 @@ protected final int myDamage;
 protected double mySpeed;
 protected Player myPlayer;
 protected Long startTime;
+protected final long LIFETIME = 10000;
 
   public class Bullet(Player player)
   {
@@ -22,6 +23,28 @@ protected Long startTime;
   public Player getPlayer()
   {
     return myPlayer;
+  }
+  
+  public boolean expired (){
+    return (System.currentTimeMillis() - startTime >= LIFETIME);
+  }
+  
+  public long time()
+  {
+    return System.currentTimeMillis - startTime;
+  }
+  
+  public void act()
+  {
+    if(this.time() >= LIFETIME)
+    {
+      this.destroy();
+    }
+  }
+  
+  public void destroy()
+  {
+    //needs implementation
   }
 
 }
