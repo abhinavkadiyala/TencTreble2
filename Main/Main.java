33973c7b2494d6b3791947dbc08da02840d9b0c8
Davidmao421 <davidmao421@gmail.com>
2015-05-18 09:04:32 -0700

    Update Main.java
    Still need to modify

--- Main/Main.java
+++ Main/Main.java
@@ -1,10 +1,33 @@
+import java.util.*;
 
+public class TimerDemo {
+   public static void main(String[] args) {
+   // creating timer task, timer
+   TimerTask tasknew = new TimerSchedulePeriod();
+   Timer timer = new Timer();
+      
+   // scheduling the task at interval
+   timer.schedule(tasknew,100, 100);      
+   }
+   // this method performs the task
+   public void run() {
+   System.out.println("timer working");      
+   }    
+}
 public class Main {
 
 	public static void main(String[] args) {
+		Game game = new Game();
+		GUI gui = new GUI();// STILL NEED TO CHANGE BOTH OF THESE BEACUSE UNSURE OF CONSTRUCTORS
 		// TODO Auto-generated method stub
-
-		System.out.println("This our game do yolo swag daddy");
+		TimerTask periodicTask = new TimerSchedulePeriod();
+		Timer timer = new Timer();
+		timer.schedule(tasknew, 0, 20);
+	}
+	public void run(){
+		game.update();
+		gui.update();
+	}
 	}
 
 }
