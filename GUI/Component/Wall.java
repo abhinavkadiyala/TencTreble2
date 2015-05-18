fd1128626e229f0d6d85a505b285ddb0dc2d8fd6
justindli <justindli@users.noreply.github.com>
2015-05-18 08:20:09 -0700

    Update Wall.java

--- GUI/Component/Wall.java
+++ GUI/Component/Wall.java
@@ -1,5 +1,23 @@
 package Component;
 
-public class Wall {
+public class Wall extends GameObject{
+  int myDirection;
+  Point2D.Double myLoc;
+  
+  public Wall(int direction, Point2D.Double loc)
+  {
+    myDirection = direction;
+    myLoc = loc;
+  }
+  
+  public int getDirection()
+  {
+    return myDirection;
+  }
+  
+  public Point2D.Double getLocation()
+  {
+    return myLoc;
+  }
 
 }
