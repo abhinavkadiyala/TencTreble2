756d867e76fa4b307ea6739f717db9ee6714547d
justindli <justindli@users.noreply.github.com>
2015-05-18 09:08:44 -0700

    Update and rename Bullet to Bullet.java

--- GUI/Component/Bullet.java
+++ GUI/Component/Bullet.java
@@ -5,7 +5,7 @@ import java.awt.geom.Point2D;
 public abstract class Bullet extends GameObject
 {
 protected final int myDamage;
-protected int mySpeed;
+protected double mySpeed;
 protected Player myPlayer;
 protected Long startTime;
 
