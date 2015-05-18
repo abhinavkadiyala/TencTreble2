6f33296f3f86d3b66056fbbe09aab1dee11670c7
justindli <justindli@users.noreply.github.com>
2015-05-18 08:48:48 -0700

    Update Wall.java
    Removing GameObject variables and methods

--- GUI/Component/Wall.java
+++ GUI/Component/Wall.java
@@ -3,23 +3,11 @@ package Component;
 import java.awt.geom.Point2D;
 
 public class Wall extends GameObject{
-  int myDirection;
-  Point2D.Double myLoc;
   
   public Wall(int direction, Point2D.Double loc)
   {
     myDirection = direction;
     myLoc = loc;
-  }
-  
-  public int getDirection()
-  {
-    return myDirection;
-  }
-  
-  public Point2D.Double getLocation()
-  {
-    return myLoc;
   }
 
 }
