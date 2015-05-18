c9dfd8d2fd05243d969d609e11ea7f0143cd27d4
Davidmao421 <davidmao421@gmail.com>
2015-05-18 09:14:15 -0700

    Update PowerUp.java

--- GUI/Component/PowerUp.java
+++ GUI/Component/PowerUp.java
@@ -3,9 +3,8 @@ impot java.awt.geom.Point2D;
 
 public class PowerUp extends GameObject{
   protected Power myPower;
-  public PowerUp(Point2D.Double() loc, Power power){
-    myLoc = loc;
+  public PowerUp(Point2D.Double() loc, Power power, Map amap){
+    super(loc, 0, amap);
     myPower = power;
-    myDirection = 0;
   }
 }
