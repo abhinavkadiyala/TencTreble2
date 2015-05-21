2367b38f53614fafdf23003e2fec6238edf8c893
justindli <justindli@users.noreply.github.com>
2015-05-21 11:00:59 -0700

    Update KillBullet.java

--- GUI/Component/KillBullet.java
+++ GUI/Component/KillBullet.java
@@ -5,8 +5,8 @@ import java.awt.geom.Point2D;
 public class KillBullet extends Bullet{
 public KillBullet(Player player)
 {
+  super(player);
   myDamage = 1;
-  mySpeed = 1; //change value as needed
 }
 
 }
