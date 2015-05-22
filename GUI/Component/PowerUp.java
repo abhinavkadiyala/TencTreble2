7da75019c8d6451f051bdcdb81495f0a0f2ea488
cdr135 <cdr135@users.noreply.github.com>
2015-05-22 10:25:13 -0700

    Fix formatting

--- GUI/Component/PowerUp.java
+++ GUI/Component/PowerUp.java
@@ -1,10 +1,10 @@
-Package Component;
-impot java.awt.geom.Point2D;
+package Component;
+import java.awt.geom.Point2D;
 
 public class PowerUp extends GameObject{
-  protected Power myPower;
-  public PowerUp(Point2D.Double() loc, Power power, Map amap){
-    super(loc, 0, amap);
-    myPower = power;
-  }
+	protected Power myPower;
+	public PowerUp(Point2D.Double loc, Power power, Map amap){
+		super(loc, 0, amap);
+		myPower = power;
+	}
 }
