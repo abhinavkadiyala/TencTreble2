fe8ac72098c5069bb4c167599717662706e9fdfd
cdr135 <cdr135@users.noreply.github.com>
2015-05-22 11:07:42 -0700

    Update Map.java

--- GUI/Component/Map.java
+++ GUI/Component/Map.java
@@ -1,17 +1,15 @@
 package component;
 
-public class Map{
-  HashSet<GameObject> obj;
+public class Map {
+	Set<GameObject> obj;
+	public Map() {
+		obj = new HashSet<GameObject> obj;
+	}
   
-  public Map (){
-    obj = new HashSet<GameObject> obj;
-  }
-  
-  public void add (GameObject gObj){
-      obj.add(gObj);
-  }
-  public void remove (GameObject obj){
-      obj.remove(gObj);
-    }
-
+	public void add(GameObject gObj) {
+		obj.add(gObj);
+	}
+	public void remove(GameObject gObj) {
+		obj.remove(gObj);
+	}
 }
