001ea60c37d112e752bf8e1218d5228e57a9da4c
cdr135 <cdr135@users.noreply.github.com>
2015-05-26 08:10:52 -0700

    Update Game.java

--- Game/Game.java
+++ Game/Game.java
@@ -1,13 +1,17 @@
 import java.awt.geo.Point2D;
-public class Game implements KeyListener{
+public class Game implements KeyListener {
   private Player[] players;
-  private HashSet<GameObject> obj;
+  private Set<GameObject> obj;
   // STILL NEED TO IMPLEMENT TIME
-  public Game(){
+  public Game() {
     players = new Player[];
     obj = new HashSet<GameObject>();
   }
-  public GameObject isValid(Point2D.Double loc){
+  public Game(int pn) {
+    players = new Player[pn];
+    obj = new HashSet<GameObject>();
+  }
+  public GameObject isValid(Point2D.Double loc) {
     if (obj.get(loc) != null){
       return obj.get(loc);
     }
@@ -15,7 +19,7 @@ public class Game implements KeyListener{
       return null;
     }
   }
-  public update(){
+  public update() {
     for(GameObject object: obj)
     {
       object.update();
