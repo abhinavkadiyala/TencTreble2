cf2ffc6d142f30d03b0df30244fd3f371c2adc26
Davidmao421 <davidmao421@gmail.com>
2015-05-26 09:28:11 -0700

    Update Bullet.java

--- GUI/Component/Bullet.java
+++ GUI/Component/Bullet.java
@@ -4,7 +4,7 @@ import java.awt.geom.Point2D;
 
 public abstract class Bullet extends GameObject
 {
-	protected final int myDamage;
+	protected final int myDamage = 1;
 	protected double mySpeed;
 	protected Player myPlayer;
 	protected Long startTime;
