e198631178614fec5ddd0513d08d74cac68f468c
cdr135 <cdr135@users.noreply.github.com>
2015-05-26 08:43:51 -0700

    Update Bullet.java
    spaces -> tabs

--- GUI/Component/Bullet.java
+++ GUI/Component/Bullet.java
@@ -4,63 +4,52 @@ import java.awt.geom.Point2D;
 
 public abstract class Bullet extends GameObject
 {
-protected final int myDamage;
-protected double mySpeed;
-protected Player myPlayer;
-protected Long startTime;
-protected final long LIFETIME = 10000;
-protected boolean expire = false;
+	protected final int myDamage;
+	protected double mySpeed;
+	protected Player myPlayer;
+	protected Long startTime;
+	protected final long LIFETIME = 10000;
+	protected boolean expire = false;
 
-  public Bullet(Player player)
-  {
-    super(player.getTank().getLocation(), player.getTank().getDirection(), player.getTank().getMap());
-    myPlayer = player;
-    startTime = System.currentTimeMillis();
-    mySpeed = 1; //change value as needed
-  }
-  public double getSpeed()
-  {
-    return mySpeed;
-  }
+	public Bullet(Player player) {
+		super(player.getTank().getLocation(), player.getTank().getDirection(), player.getTank().getMap());
+		myPlayer = player;
+		startTime = System.currentTimeMillis();
+		mySpeed = 1; //change value as needed
+	}
+	public double getSpeed() {
+		return mySpeed;
+	}
   
-  public Player getPlayer()
-  {
-    return myPlayer;
-  }
+	public Player getPlayer() {
+		return myPlayer;
+	}
   
-  public boolean expired (){
-    return expire;
-  }
+	public boolean expired () {
+		return expire;
+	}
   
-  public long time()
-  {
-    return System.currentTimeMillis - startTime;
-  }
+	public long time() {
+		return System.currentTimeMillis - startTime;
+	}
+	
+	public void conflict(GameObject other) {
+		if(other instanceof Wall) {
+			this.setDirection(2*other.getDirection() - this.getDirection());
+		}
+	}
   
-  public void conflict(GameObject other)
-  {
-    if(other instanceof Wall)
-    {
-      this.setDirection(2*other.getDirection() - this.getDirection());
-      //physically bounce somehow
-    }
-  }
+	public void update() {
+		if(this.time() >= LIFETIME) {
+			this.destroy();
+			expire = true;
+		} else {
+			//move
+		}
+	}
   
-  public void update()
-  {
-    if(this.time() >= LIFETIME)
-    {
-      this.destroy();
-      expire = true;
-    }else
-    {
-      //move
-    }
-  }
-  
-  public void destroy()
-  {
-    //needs implementation
-  }
+	public void destroy() {
+		//needs implementation
+	}
 
 }
