29e69c1bbcb7fbbd09c11a4a4f1668905991e5c5
cdr135 <cdr135@users.noreply.github.com>
2015-05-27 09:22:18 -0700

    Update GameObject.java

--- GUI/Component/GameObject.java
+++ GUI/Component/GameObject.java
@@ -64,4 +64,6 @@ public abstract class GameObject {
 	}
 	public abstract void conflict(GameObject other);
 	public abstract void update(); //apparently it took in an int, delta time before
+	
+	public abstract void paint(Graphics g);
 }
