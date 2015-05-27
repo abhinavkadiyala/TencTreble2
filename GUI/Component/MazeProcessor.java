27773765e8dc025fb8f7007d27990c3c6c85e373
abhinavkadiyala <abhinav_kadiyala@yahoo.com>
2015-05-26 21:45:03 -0700

    Generates maze gui from mazeprocessor

--- GUI/Component/MazeProcessor.java
+++ GUI/Component/MazeProcessor.java
@@ -1,10 +1,33 @@
 public class MazeProcessor
 {
-  public static Maze generate(String maze)
+  
+  private Maze myMaze;
+  
+  public MazeProcessor(Maze a)
   {
+    myMaze = a;
+  }
+  
+  public Maze generate(String maze)
+  {
+    int col = 0;
+    
     for(int i = 0; i < maze.length(); i++)
     {
+      if(maze.charAt(i) == '-')
+      {
+        putWallAt(i,col);
+      }
       
+      if(maze.charAt(i) == '\n')
+      {
+        ++col;
+      }
     }
+  }
+  
+  public putWallAt(int row, int col)
+  {
+    
   }
 }
