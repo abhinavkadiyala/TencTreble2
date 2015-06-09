c1bf82ceb6bd72e5a7d68b8a8ae7bd2b29ac60a7
cdr135 <cdr135@users.noreply.github.com>
2015-06-09 08:26:06 -0700

    Update GamePanel.java

--- GUI/GamePanel.java
+++ GUI/GamePanel.java
@@ -93,6 +93,14 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		g.translate(Game.CELL_SIDE, Game.CELL_SIDE);
 		game.paint((Graphics2D) g);
 	}
+	
+	public String code() {
+		return Game.code()
+	}
+	public void setCode(String code) {
+		game = new Game(code);
+		newGame();
+	}
 
 }
 
